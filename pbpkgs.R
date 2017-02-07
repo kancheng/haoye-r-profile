707e30122978d6fd865bb57a06db7e11ab07a674
kancheng <zxdfgcv@gmail.com>
2017-02-07 21:24:25 +0800

    kan edit

---
+++ pbpkgs.R
@@ -0,0 +1,9 @@
+﻿
+pkgs = c("devtools", "lattice", "GGally", "lubridate", "stringr", "ggplot2", "scales", "reshape2", "data.table", "coefplot", "broom")
+
+pkgs = pkgs[!( pkgs %in% installed.packages()[,"Package"] )]
+
+# install
+if(length(pkgs)){
+	install.packages(pkgs)
+}
